0159f1f5848b9c611d3c8ccecaaf4ddacb4a9bb8
Septian Padli <muhammadpadliseptiana@gmail.com>
2023-03-08 14:54:15 +0700

    update dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Lelang;
+use App\Models\Asset;
+
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('pages.admin.dashboard');
+        $user = User::where('is_admin', 0)->get()->count();
+        $lelang = Lelang::where('status', 1)->get();
+        $asset = Asset::all();
+        return view('pages.admin.dashboard', [
+            'user' => $user,
+            'lelang' => $lelang,
+            'asset' => $asset,
+        ]);
     }
 }
--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -29,8 +29,8 @@
                       <i class="bi bi-box-seam"></i>
                     </div>
                     <div class="ps-3">
-                      <h6>19</h6>
-                      <span class="text-muted small pt-2 ps-1">Total Lelang Berjalan</span>
+                      <h6>{{$lelang->count()}}</h6>
+                      <span class="text-muted small pt-2 ps-1">Lelang Sedang Berjalan</span>
                     </div>
                   </div>
                 </div>
@@ -51,7 +51,7 @@
                       <i class="bi bi-people"></i>
                     </div>
                     <div class="ps-3">
-                      <h6>999</h6>
+                      <h6>{{$user}}</h6>
                       <span class="text-muted small pt-2 ps-1">Total Pengguna</span>
                     </div>
                   </div>
@@ -66,15 +66,15 @@
               <div class="card info-card revenue-card">
 
                 <div class="card-body">
-                  <h5 class="card-title">Penawaran Tertinggi</h5>
+                  <h5 class="card-title">Produk</h5>
 
                   <div class="d-flex align-items-center">
                     <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
                       <i class="bi bi-cash"></i>
                     </div>
                     <div class="ps-3">
-                      <h6>Rp. <span class="harga">20000000</span></h6>
-                      <span class="text-muted small pt-2 ps-1">Penawaran tertinggi</span>
+                      <h6>{{$asset->count()}}</h6>
+                      <span class="text-muted small pt-2 ps-1">Total Produk Ditambahkan</span>
                     </div>
                   </div>
                 </div>
@@ -82,10 +82,10 @@
               </div>
             </div><!-- End Tertinggi Card -->
 
-            
+
           </div>
         </div><!-- End columns -->
-        
+
         <!-- Penawaran Terbaru -->
         <div class="col-lg-12">
           <div class="card top-selling overflow-auto">
